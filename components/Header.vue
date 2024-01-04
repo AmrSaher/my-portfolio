9883493f18ef02d794aa8cfe0690a82d4e07dc6e
Amr Saher <amrosaher1234@gmail.com>
2024-01-04 15:23:27 -0800

    fix some bugs in header

--- components/Header.vue
+++ components/Header.vue
@@ -2,18 +2,23 @@
     <header
         class="
             Header fixed top-8 left-[50%] translate-x-[-50%]
-            px-3 h-[60px] border border-[#404040b5] rounded-[2rem]
-            bg-[#72727233] flex items-center text-[#ffffffe5]
-            z-50
+            px-3 h-[60px] rounded-[2rem] flex items-center
+            text-[#ffffffe5] z-50 transition-[background]
         "
+        :class="{
+            'border border-[#404040b5] bg-[#72727233]': showBg,
+        }"
     >
         <nav class="w-full">
             <span
                 class="
-                    w-[100px] bg-[#4f4f4f66] h-[40px] rounded-3xl
+                    w-[100px] h-[40px] rounded-3xl
                     absolute -z-10 transition-all
                 "
-                :class="`left-[${sliderX}px]`"
+                :class="{
+                    'bg-[#4f4f4f66]': showBg,
+                }"
+                :style="`left: ${sliderX}px`"
             ></span>
             <ul class="w-full flex items-center gap-3">
                 <li class="w-[100px] h-[40px] flex items-center justify-center">
@@ -32,8 +37,17 @@
 
 <script setup>
 const sliderX = ref(12)
+const showBg = ref(false)
 
+// Highlight active links
 const navigate = (n) => {
     sliderX.value = 12 + (n - 1) * 112
 }
+
+// Show header after scrolling
+onMounted(() => {
+    window.addEventListener('scroll', () => {
+        showBg.value = window.scrollY > 6
+    })
+})
 </script>
